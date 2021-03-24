935fba783a0f574b94cad2639e2c0d817af8b488
Scott Luxton <78744890+ScottLuxton@users.noreply.github.com>
2021-03-24 11:42:57 -0600

    Add files via upload

---
+++ catTreatGenerator.js
@@ -0,0 +1,15 @@
+const catTreats = [
+    'Turkey',
+    'Salmon',
+    'Tuna',
+    'Beef',
+    'Cheese',
+]
+
+const catTreatGenerator = catTreats[Math.floor(Math.random() * catTreats.length)];
+
+console.log(catTreatGenerator)
+
+
+
+
